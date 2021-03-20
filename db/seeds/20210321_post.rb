92d93692225b706a31b21b2265fbe3f05a299016
KeiTakeda <kt10195665@yahoo.ne.jp>
2021-03-20 23:03:16 +0000

    ダンプファイルのプッシュ

---
+++ db/seeds/20210321_post.rb
@@ -0,0 +1,8 @@
+Post.create!([
+  {content: "問題内容", title: "テスト１", answer: nil, content_ja: nil},
+  {content: "testtesttesttesttesttesttesttest", title: "テスト２", answer: nil, content_ja: nil},
+  {content: "入国の目的はなんですか？\r\nwhat's the purpose of your visit?\r\n\r\n観光です\r\n[         ]", title: "理由を説明する", answer: nil, content_ja: nil},
+  {content: "Excuse me, but [???]", title: "場所を尋ねる", answer: "could you tell me the way to the nearest bank?", content_ja: nil},
+  {content: "{???} Mr.Takahashi?", title: "電話をかける", answer: "May I talk to", content_ja: nil},
+  {content: "{???} Mr.Takahashi?", title: "電話をかける２", answer: "May I talk to", content_ja: "高橋さん、いらっしゃいますか？"}
+])
